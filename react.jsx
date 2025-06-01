a368ceaee315f94b2c6b8246df7556a0898fb5a0
syt4himanshu <mirehimanshu@gmail.com>
2025-06-01 19:20:09 +0530

    ok done

---
+++ react.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+function Counter(){
+    const [count, setCount]=useState(0);
+    return (
+        <div>
+            <h1>count:{count}</h1>
+            <button onClick={()=>setCount(count + 1)}>Increment</button>
+        </div>
+    );
+ }
+ export default Counter;
